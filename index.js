db772871b6dc1af3d806636a37f4a34b881e46a4
guerino antonini <guerantonini94@gmail.com>
2021-09-13 19:44:35 -0400

    WIP: added renderFilms to show list on webpage. Added comment text area and render on page.

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-// fetch("https://swapi.dev/api/films", {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'appliction/json'
-//     },
-//     body: JSON.stringify({
-//         title
-//     })     
-// })
-document.addEventListener('DOMContentLoaded', function() {
-    fetchMovies()
-  })
 
-  function fetchMovies() {
-    return fetch("https://swapi.dev/api/films")
-      .then(resp => resp.json())
-      .then(json => renderMovies(json))
-  }
+const main = document.querySelector('main')
+const commentList = document.getElementById('comment_container')
 
 
-  function renderMovies(movies) {
-  const body = document.querySelector('body')
-  console.log(body)
-  movies.forEach(movie => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = movie.title
-    body.appendChild(h2)
-  })
+function fetchBooks() {
+  return fetch("https://swapi.dev/api/films")
+    .then(resp => resp.json())
+    .then(json => renderFilms(json.results))
 }
+
+function renderFilms(films) {
+  
+  films.forEach(film => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = film.title
+    main.appendChild(h2)
+  });
+}
+
+function renderComment(com){
+  let li = document.createElement('li')
+  li.textContent = com
+  commentList.appendChild(li)
+}
+    
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  renderComment(e.target.comment.value)
+})
+
